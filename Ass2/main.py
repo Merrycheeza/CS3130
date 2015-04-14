d12dbe729feb2c05887ee1fe5044182185dc2bcd
Samara Drewe <merrycheeza@gmail.com>
2015-04-14 12:47:54 -0600

    Create main.py

---
+++ Ass2/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+###################################
+#				  
+# Class: 	CS3130		  
+# Assignment: 	Assignment 1      
+# Author:	Samara Drewe	  
+# 		4921860		  
+#				  
+###################################
+
+import functions
+
+def main():
+
+    functions.title()
+    text = functions.getFile()
+    functions.process(text)
+
+main()
